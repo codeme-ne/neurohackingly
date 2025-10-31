f2726a4c746e6a29d65fef2f35c7640de065ad26
codeme-ne <lukas.zangerl@mail.fernfh.ac.at>
2025-10-31 21:41:28 +0100

    convertkit fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://neurohackingly.com',
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true
